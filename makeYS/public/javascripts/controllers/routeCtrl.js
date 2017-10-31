e751edea419bcc590c3d4d39227792f8c8cea4b5
MossyChuck <ipasha1975@gmail.com>
2017-11-01 00:35:35 +0300

    navigation
    some navigation in header

--- makeYS/public/javascripts/controllers/routeCtrl.js
+++ makeYS/public/javascripts/controllers/routeCtrl.js
@@ -1,7 +1,10 @@
 angular.module("app").controller("routeCtrl",function($scope){
-    $scope.url = "/htmls/signUp.html";
+    $scope.url = "/htmls/main.html";
     $scope.$on('changeUrl', function(event,args){
         $scope.url = args.url;
         $scope.message = args.message;
     });
+    $scope.changeUrl = function(url){
+        $scope.url = url;
+    }
 });
