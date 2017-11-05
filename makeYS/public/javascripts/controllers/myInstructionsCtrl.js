0fa2cdcfe32e6665738f316985277547edfdbc88
MossyChuck <ipasha1975@gmail.com>
2017-11-05 19:01:29 +0300

    table sort in my instructions

--- makeYS/public/javascripts/controllers/myInstructionsCtrl.js
+++ makeYS/public/javascripts/controllers/myInstructionsCtrl.js
@@ -1,3 +1,5 @@
 angular.module('app').controller('myInstructionsCtrl',function($scope){
     $scope.instructions = instructionModel.getInstructionsByAuthor(sessionStorage.userId);
+    $scope.sortType = 'id';
+    $scope.sortReverse = false;
 });
