077d86d0bbda49d54c0c85a4c81f5397d4553f27
MossyChuck <ipasha1975@gmail.com>
2017-11-08 01:02:37 +0300

    admin page

---
+++ makeYS/public/javascripts/controllers/adminPageCtrl.js
@@ -0,0 +1,56 @@
+angular.module('app').controller('adminPageCtrl',function ($scope,$http) {
+    $scope.users = userModel.data;
+    $scope.sortType = 'id';
+    $scope.sortReverse = false;
+    $scope.selected;
+    $scope.limitValue = 3;
+    $scope.usersPerPage = 3;
+    $scope.getBlockButtonText = function (id) {
+        return userModel.getUserById(id).blocked ? 'Unblock' : 'Block';
+    }
+    $scope.getAdminButtonText = function (id) {
+        return userModel.getUserById(id).admin ? 'Make not admin' : 'Make admin';
+    }
+    $scope.select = function (event) {
+        if($scope.selected == this.$index){
+            $scope.selected = undefined;
+        }else{
+            $scope.selected = this.$index;
+        }
+    }
+    $scope.bool = function(expression){
+        return expression ? 'Yes' : 'No';
+    }
+    $scope.changeBlockState = function (id) {
+        userModel.changeProperty($http,id,'blocked',1 - userModel.getUserById(id).blocked);
+        userModel.load($http);
+        if(id == sessionStorage.userId) {
+            delete sessionStorage.role;
+            delete sessionStorage.userId;
+            $scope.$emit('changeHeaderUrl', { url: '/htmls/headers/nonAutorized.html' });
+            $scope.$emit('changeContentUrl', { url: 'htmls/content/main.html' });
+        }
+        $scope.users.forEach(function (element) {
+            if(element.id == id){
+                element.blocked = 1 - element.blocked;
+            }
+        }, this);
+    }
+    $scope.changeAdminState = function (id) {
+        userModel.changeProperty($http,id,'admin',1 - userModel.getUserById(id).admin);
+        userModel.load($http);
+        if(id == sessionStorage.userId){
+            sessionStorage.role = 'user';
+            $scope.$emit('changeContentUrl', { url: 'htmls/content/main.html' });
+        }
+        $scope.users.forEach(function (element) {
+            if(element.id == id){
+                element.admin = 1 - element.admin;
+            }
+        }, this);
+    }
+    $scope.openProfile = function (id) {
+        localStorage.profileId = id;
+        $scope.$emit('changeContentUrl',{ url: '/htmls/content/profile.html'})
+    }
+});
--- makeYS/public/javascripts/controllers/routeCtrl.js
+++ makeYS/public/javascripts/controllers/routeCtrl.js
@@ -2,24 +2,27 @@ angular.module("app").controller("routeCtrl",function($scope, $http) {
     userModel.load($http).then(function() {
         $scope.$digest();
     });
-    instructionModel.load($http,$scope).then(function() {
+    instructionModel.load($http,$scope).then(function () {
         $scope.$digest();
     });
+    $scope.isAdmin = function(){
+        return sessionStorage.role=='admin' ? true : false;
+    }
     $scope.contentUrl = "/htmls/content/main.html";
     $scope.headerUrl = sessionStorage.length > 0?"/htmls/headers/autorizedUser.html" : "/htmls/headers/nonAutorized.html";
-    $scope.$on('changeContentUrl', function(event,args) {
+    $scope.$on('changeContentUrl', function (event,args) {
         $scope.contentUrl = args.url;
         $scope.message = args.message;
     });
-    $scope.$on('changeHeaderUrl', function(event,args) {
+    $scope.$on('changeHeaderUrl', function (event,args) {
         $scope.headerUrl = args.url;
     });
-    $scope.logout = function() {
+    $scope.logout = function () {
         delete sessionStorage.role;
         delete sessionStorage.userId;
         $scope.headerUrl = "/htmls/headers/nonAutorized.html";
     }
-    $scope.changeContentUrl = function(url) {
+    $scope.changeContentUrl = function (url) {
         $scope.contentUrl = url;
     }
     
