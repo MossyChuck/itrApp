bde168099348c83f26d647907260632b7fa18a86
MossyChuck <ipasha1975@gmail.com>
2017-11-08 15:12:16 +0300

    pdf downloading

--- makeYS/public/javascripts/controllers/instructionCtrl.js
+++ makeYS/public/javascripts/controllers/instructionCtrl.js
@@ -8,10 +8,22 @@ angular.module('app').controller('instructionCtrl',function($scope,$http){
             step.imagesLinks.forEach(function(element) {
                 dbx.sharingGetSharedLinkFile({url: element})
                 .then(function(data) {
-                    console.log(data);
-                    step.img.push(URL.createObjectURL(data.fileBlob));
-                    element = URL.createObjectURL(data.fileBlob);
-                    $scope.$digest();
+                    //var file = new File([data.fileBlob],data.name,{type: 'image/jpeg', lastModified: Date.now()});
+                    
+                    var reader = new window.FileReader();
+                    var extention = data.name.substring(data.name.lastIndexOf('.')+1);
+                    reader.readAsDataURL(data.fileBlob); 
+                    reader.onloadend = function() {
+                                   var base64data = reader.result.replace('application/octet-stream','image/'+extention);
+                                   step.img.push(base64data);
+                                   $scope.$digest();
+                                   
+                     }
+                     
+                    //step.img.push(URL.createObjectURL(file));
+                    //step.img.push(URL.createObjectURL(data.fileBlob));
+                    //element = URL.createObjectURL(data.fileBlob);
+                    
                 })
                 .catch(function(error) {
                     console.error(error);
@@ -19,26 +31,41 @@ angular.module('app').controller('instructionCtrl',function($scope,$http){
             }, this);
         }
     }, this);
-    $scope.profile = function(id) {
+    $scope.profile = function (id) {
         localStorage.profileId = id;
         $scope.$emit('changeContentUrl', { url: '/htmls/content/profile.html'});
     }
-    $scope.getUsername = function(id) {
+    $scope.getUsername = function (id) {
         return userModel.getUserById(id).username;
     }
-    $scope.isAuthor = function(id){
+    $scope.isAuthor = function (id) {
         if(id==sessionStorage.userId || sessionStorage.role == 'admin'){
             return true;
         } else{
             return false;
         }
     }
-    $scope.editInstruction = function(id){
+    $scope.editInstruction = function (id) {
         localStorage.instructionId = id;
         $scope.$emit('changeContentUrl',{ url: '/htmls/content/editInstruction.html'});
     }
-    $scope.deleteInstruction = function(id){
+    $scope.deleteInstruction = function (id) {
         instructionModel.deleteInstructionById($http,id);
         $scope.$emit('changeContentUrl', { url: '/htmls/content/main.html'});
+    }
+    $scope.generatePDF = function () {
+        var draw = kendo.drawing;
+        draw.drawDOM($('#pdf'), {
+            avoidLinks: true,
+            paperSize: 'A4',
+            margin: '0.5cm'
+        }).then(function (root) {
+            return draw.exportPDF(root,10,10);
+        }).done(function (data) {
+            kendo.saveAs({
+                dataURI: data,
+                fileName: $scope.instruction.title+'.pdf'
+            });
+        });
     }
 });
